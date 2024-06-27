48633b8f6024a99fb180c3afbb09ce3812ceba91
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2024-06-27 13:52:37 -0500

    exploring go

---
+++ variables/variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declaring variables
+	var a int = 10
+	var b = 20 // Type inferred
+	c := 30    // Short declaration, can only be used in functions
+
+	fmt.Println("a:", a)
+	fmt.Println("b:", b)
+	fmt.Println("c:", c)
+}
